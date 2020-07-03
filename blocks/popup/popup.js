23f9cd1727940936e5a1b0c835080475793ec2bb
sorad <soradimichi@gmail.com>
2020-07-03 07:41:25 +0300

    Финальные шаги
    Signed-off-by: sorad <soradimichi@gmail.com>

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,59 +1,91 @@
+const popup = document.querySelector('.popup') //Найдем попап
+const form = popup.querySelector('.popup__form') //Найдем форму в попап
+
+const formInputName = form.querySelector('.popup__formInputText_typeName') //Найдем инпут имени в форме
+const formInputDescription = form.querySelector('.popup__formInputText_typeDescription') //Найдем инпут описания в форме
+
+const outputName = document.querySelector('.profile__name') //Найдем аутпут имени
+const outputDescription = document.querySelector('.profile__description') //Найдем аутпут описания
+
+
+//Открытие по кнопке с карандашом и запись имени и описания в поле инпутов
+
+const popupOpenButton = document.querySelector('.profile__openPopupButton') // Найдем кнопку с карандашом
+
+const popupOpen = function () {
+    formInputName.value = outputName.textContent
+    formInputDescription.value = outputDescription.textContent
+    popup.classList.add('popup_opened')
+    popup.classList.remove('popup_hidden')
+}
+
+popupOpenButton.addEventListener('click', popupOpen)
+
+//Закрытие по крестику
+
+const popupCloseButton = popup.querySelector('.popup__closeButton') // Найдем кнопку крестик
+
+const popupClose = function () {
+    popup.classList.add('popup_hidden')
+    popup.classList.remove('popup_opened')
+}
+
+popupCloseButton.addEventListener('click', popupClose) //Повесим функцию на крестик
+
+//Закрытие при клике по любому месту кроме popup__form
+
+const closePopupByClickingOverlay = function (event) {
+    if (event.target !== event.currentTarget) { return }
+    popupClose()
+}
+
+popup.addEventListener('click', closePopupByClickingOverlay) //Повесим функцию на пространство вне формы
+
+//Отправка формы (проверка инпутов не доделана)
+
+const formSubmitButton = form.querySelector('.popup__formSubmitButton') //Найдем кнопку сабмита
+
+function ChangeNameAndDescription () {
+
+if (confirm(`Вы ввели:
+Имя: ${formInputName.value}
+Описание: ${formInputDescription.value}
+Подтвердите правильность ввовда:`) === true)  {
+outputName.textContent = formInputName.value
+outputDescription.textContent = formInputDescription.value
+    function formSubmitHandler (event) {
+        event.preventDefault()
+        outputName.textContent = formInputName.value
+        outputDescription.textContent = formInputDescription.value
+    }
+    formSubmitHandler()
+popupClose()
+} else {
+    return
+}
+}
+
+formSubmitButton.addEventListener('click', ChangeNameAndDescription)
+
+
+
+// //Проверка форм на количество символов
 //
+// function formChecker() {
 //
+//     let proveName = formInputName.addEventListener('keyup', checkName(evt) {
+//         return (formInputName.value.length > 0)
+//     })
 //
+//     let proveDescription = formInputDescription.addEventListener('keyup', checkDescription(evt) {
+//         return (formInputDescription.value.length > 0)
+//     })
 //
-// const formInputName = popup.querySelector('.form__inputText_typeName')
-// const formInputDescription = popup.querySelector('.form__inputText_typeDescription')
-// const outputName = document.querySelector('.profile__name')
-// const outputDescription = document.querySelector('.profile__description')
-//
-//
-//
-//
-// // Могли бы вы помочь, пожалуйста
-// // https://imgur.com/a/p4oE1xb
-// // В тренажере был предоставлен метод вызова value (ссылка), я проковырялся так и не понял почему у меня он не работает,
-// // хотел сделать проверку на длинну в инпутах через formInputDescription.value.length и
-// // ${formInputDescription.value.length} для включения кнопки - не работает,
-// // так же пытался для всех остальных задач использовать. Помогите, пожалуйста, расскажите как пользоваться :c
-//
-//
-// const popup = document.querySelector('.popup')
-// const popupOpenButton = document.querySelector('.profile__openPopupButton')
-// const popupCloseButton = popup.querySelector('.popup__closeButton')
-// const formSubmitButton = popup.querySelector('.form__submitButton')
-//
-// //Код для включения кнопки после проверки инпутов на будущее
-// formSubmitButton.removeAttribute('disabled');
-// formSubmitButton.classList.remove('form__submitButton_disabled')
-// formSubmitButton.classList.add('form__submitButton_enabled')
-// //Код для включения кнопки по проверке инпутов на будущее
-//
-// let changeNameAndDescription = function() {
-//     outputName.innerHTML = formInputName.value
-//     outputDescription.innerHTML = formInputDescription.value
-//     popupClose()
+//     if (proveDescription && proveName = true){
+//         return
+//     } else {
+//         formSubmitButton.setAttribute('disabled');
+//         formSubmitButton.classList.add('form__submitButton_disabled')
+//         formSubmitButton.classList.remove('form__submitButton_enabled')
+//     }
 // }
-//
-// formSubmitButton.addEventListener('click', changeNameAndDescription);
-//
-// const popupOpen = function () {
-//     formInputName.value = outputName.innerHTML
-//     formInputDescription.value = outputDescription.innerHTML
-//     popup.classList.add('popup_opened')
-//     popup.classList.remove('popup_hidden')
-// }
-//
-// const popupClose = function () {
-//     popup.classList.add('popup_hidden')
-//     popup.classList.remove('popup_opened')
-// }
-//
-// const closePopupByClickingOverlay = function (event) {
-//     if (event.target !== event.currentTarget) { return }
-//     popupClose()
-// }
-//
-// popup.addEventListener('click', closePopupByClickingOverlay)
-// popupOpenButton.addEventListener('click', popupOpen)
-// popupCloseButton.addEventListener('click', popupClose)
